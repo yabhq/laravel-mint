ed052cb05c76e11ee900734f88511afed7259245
Jim <jimhlad@gmail.com>
2020-06-25 12:49:43 -0400

    Slugify trait

---
+++ src/Traits/Slugify.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Yab\Mint\Traits;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Query\Builder;
+
+trait Slugify
+{
+    public static function bootSlugify()
+    {
+        static::creating(function ($model) {
+            if (is_null($model->slug)) {
+                $model->slug = Str::slug($model->name);
+            }
+            $model->slug = self::checkModelSlug($model->slug);
+        });
+
+        static::updating(function ($model) {
+            if ($model->getOriginal('slug') !== $model->slug) {
+                $model->slug = self::checkModelSlug($model->slug);
+            }
+        });
+    }
+
+    /**
+     * Check that the slug doesn't already exist if updating
+     *
+     * @param string $slug
+     *
+     * @return string
+     */
+    public static function checkModelSlug(string $slug): string
+    {
+        if (self::slugExists($slug)) {
+            return $slug . '-' . uniqid();
+        }
+        return $slug;
+    }
+
+    /**
+     * Check if the slug exists
+     *
+     * @param string $slug
+     *
+     * @return bool
+     */
+    public static function slugExists(string $slug): bool
+    {
+        return self::withoutGlobalScopes()->where('slug', $slug)->exists();
+    }
+
+    /**
+     * Return the model by the slug
+     *
+     * @param \Illuminate\Database\Query\Builder $scope
+     * @param string $slug
+     *
+     * @return \Illuminate\Database\Query\Builder
+     */
+    public function scopeBySlug(Builder $scope, string $slug): Builder
+    {
+        return $scope->where('slug', $slug);
+    }
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+}
---
+++ tests/Factories/SlugModelFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Yab\Mint\Tests\Models\SlugModel;
+
+$factory->define(SlugModel::class, function () {
+    return [
+        'name' => 'Sample Name'
+    ];
+});
